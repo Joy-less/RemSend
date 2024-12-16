78ce4e4973be96247817b8f1a6e81f54a9abbfbe
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-12-16 17:39:45 +0000

    Change RemPacket to struct

--- addons/RemSend/RemPacket.cs
+++ addons/RemSend/RemPacket.cs
@@ -6,11 +6,11 @@ using MemoryPack;
 namespace RemSend;
 
 [MemoryPackable]
-internal sealed partial class RemPacket(string TargetPath, string MethodName, byte[][] PackedArguments) {
-    public string TargetPath = TargetPath;
-    public string MethodName = MethodName;
-    public byte[][] PackedArguments = PackedArguments;
-    public long PacketId = Interlocked.Increment(ref LastPacketId);
+internal readonly partial struct RemPacket(string TargetPath, string MethodName, byte[][] PackedArguments) {
+    public string TargetPath { get; } = TargetPath;
+    public string MethodName { get; } = MethodName;
+    public byte[][] PackedArguments { get; } = PackedArguments;
+    public long PacketId { get; } = Interlocked.Increment(ref LastPacketId);
 
     private static long LastPacketId;
 }
