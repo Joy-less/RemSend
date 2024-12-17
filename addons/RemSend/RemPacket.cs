b3343c2c3dc27e746dfdf9d593358d99db59f3d5
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-12-17 02:04:52 +0000

    Fix rem packet deserialized incorrectly
    When it's read-only, PacketId is not set correctly. MemoryPack bug? Whatever. Let's just make it a mutable struct! And a record too!

--- addons/RemSend/RemPacket.cs
+++ addons/RemSend/RemPacket.cs
@@ -6,11 +6,11 @@ using MemoryPack;
 namespace RemSend;
 
 [MemoryPackable]
-internal readonly partial struct RemPacket(string TargetPath, string MethodName, byte[][] PackedArguments) {
-    public string TargetPath { get; } = TargetPath;
-    public string MethodName { get; } = MethodName;
-    public byte[][] PackedArguments { get; } = PackedArguments;
-    public long PacketId { get; } = Interlocked.Increment(ref LastPacketId);
+internal partial record struct RemPacket(string TargetPath, string MethodName, byte[][] PackedArguments) {
+    public string TargetPath { get; set; } = TargetPath;
+    public string MethodName { get; set; } = MethodName;
+    public byte[][] PackedArguments { get; set; } = PackedArguments;
+    public long PacketId { get; set; } = Interlocked.Increment(ref LastPacketId);
 
     private static long LastPacketId;
 }
