5e56a638df3c350ac2ce708353985f9e6c4cc909
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-02-15 23:39:27 +0000

    Add ContinueWithOnSameThread extension method

--- RemSend/RemSendExtensions.cs
+++ RemSend/RemSendExtensions.cs
@@ -4,12 +4,24 @@
 /// A set of extension methods for use with RemSend.
 /// </summary>
 public static class RemSendExtensions {
-    /// <inheritdoc cref="Task.ContinueWith(Action{Task})"/>
+    /// <summary>
+    /// Waits for the task to complete and calls <paramref name="Callback"/> on an arbitrary thread.
+    /// </summary>
     public static Task ContinueWith(this Task Task, Action Callback) {
         return Task.ContinueWith(Task => Callback());
     }
-    /// <inheritdoc cref="Task{T}.ContinueWith(Action{Task{T}})"/>
+    /// <summary>
+    /// Waits for the task to complete and calls <paramref name="Callback"/> on the original thread.
+    /// </summary>
+    public static Task ContinueWithOnSameThread(this Task Task, Action Callback) {
+        return Task.ContinueWith(Task => Callback(), TaskScheduler.FromCurrentSynchronizationContext());
+    }
+    /// <inheritdoc cref="ContinueWith(Task, Action)"/>
     public static Task ContinueWith<T>(this Task<T> Task, Action<T> Callback) {
         return Task.ContinueWith(Task => Callback(Task.Result));
+    }
+    /// <inheritdoc cref="ContinueWithOnSameThread(Task, Action)"/>
+    public static Task ContinueWithOnSameThread<T>(this Task<T> Task, Action<T> Callback) {
+        return Task.ContinueWith(Task => Callback(Task.Result), TaskScheduler.FromCurrentSynchronizationContext());
     }
 }
